1ad18b06ed7badd1b44534c48f1916b31a1340dd
MaksPikul <mmaks522@gmail.com>
2024-03-29 11:02:28 +0000

    did a drop down for create program, next up, imputing exercises into each day

--- screens/modal/ActivityHist.jsx
+++ screens/modal/ActivityHist.jsx
@@ -1,6 +1,14 @@
 import { View, Text, Button } from "react-native";
 
 export default function ActivityHist({ navigation }) {
+
+  /* 
+    Fetch request for completed days
+
+    flat list to display
+  */
+
+
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text style={{ fontSize: 30 }}>This is a modal!</Text>
--- screens/modal/ProgCreate.jsx
+++ screens/modal/ProgCreate.jsx
@@ -1,30 +1,52 @@
-import { View, Text, Button, TextInput ,SafeAreaView, StyleSheet} from "react-native";
-import { Formik } from "formik";
-import * as yup from "yup";
+import { View, Text, Button, TextInput ,SafeAreaView, StyleSheet, Modal, Alert} from "react-native";
+import SelectDropdown from 'react-native-select-dropdown'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { useState } from "react";
 import React from "react";
 
 export default function ProgCreate({ navigation }) {
   const [entered, setEntered] = useState(false)
-  const [text, onChangeText] = React.useState('');
-    
-  const valScheme = yup.object().shape({
-    name: yup.string()
-    .required("Enter a programme name")
-    .min(3, "Name too short - 3 min")
-    .max(20, "Name too large - 20 max"),
-    duration: yup.number()
-      .required("Enter a number")
-      .min(1)
-      .max(7),
-  });
+  const [data, setData] = useState({
+    name: '',
+    duration: ''
+  })
 
+  const handleSubmit = () => {
+    if (data.name === ''|| data.duration === ''){
+      Alert.alert("Enter values")
+    }
+    else{
+      console.log(data)
+    }
+  }
+
+  const handleInput = (k, v) => {
+    setData({...data, [k] : v})
+  }
+
+  const durationDrop = [
+    {title:1},
+    {title:2},
+    {title:3},
+    {title:4},
+    {title:5},
+    {title:6},
+    {title:7},
+    {title:8},
+    {title:9},
+    {title:10},
+    {title:11},
+    {title:12},
+  ]
 
   /* 
-  Once the thing is entered, display will change to screen where you choose exercises with new values, 
-
-  */
   
+  will need a conditional state, to display exercise choosing screen, 
+  either modal or bottom
+  im thinking flat list at bottom
+  */
+
+
 
   const styles = StyleSheet.create({
     input: {
@@ -36,48 +58,61 @@ export default function ProgCreate({ navigation }) {
   });
 
   return (
+  
+    <View>
+        <Text> Enter Program name</Text>
+          <TextInput style={{...styles.input}}
+          id="name"
+          name="name"
+          placeholder="My cool Programme"
+          onChangeText={(text) => handleInput('name', text)}
+          maxLength={20}
+          value={data.name}/>
 
-      
-        <Formik
-        initialValues={{name:"", duration:"1"}}
-        onSubmit={((values, actions)=> {
 
-        })}>
-          <View style={{flex:1, alignItems: "flex-start", justifyContent: "centre"}}>
+        <Text>Enter duration of programme (weeks)</Text>
+          <SelectDropdown 
+          data={durationDrop}
+          onSelect={(selectedItem,index)=>{
+            console.log(selectedItem.title);
+            handleInput('duration', String(selectedItem.title))
+          }}
+          buttonTextAfterSelection={(selectedItem) => {
+            return selectedItem;
+          }}
           
-          <TextInput style={styles.input}
-        onChangeText={onChangeText}
-        value={text}/>
+          value={data.duration}
+          renderButton={(selectedItem, isOpened) => {
+            return (
+              <View style={styles.dropdownButtonStyle}>
+                {selectedItem && (
+                  <Icon name={selectedItem.icon} style={styles.dropdownButtonIconStyle} />
+                )}
+                <Text style={styles.dropdownButtonTxtStyle}>
+                  {(selectedItem && selectedItem.title) || ''}
+                </Text>
+                <Icon name={isOpened ? 'chevron-up' : 'chevron-down'} style={styles.dropdownButtonArrowStyle} />
+              </View>
+            );
+          }}
+          renderItem={(item, index, isSelected) => {
+            return (
+              <View style={{...styles.dropdownItemStyle, ...(isSelected && {backgroundColor: '#D2D9DF'})}}>
+                <Icon name={item.icon} style={styles.dropdownItemIconStyle} />
+                <Text style={styles.dropdownItemTxtStyle}>{item.title}</Text>
+              </View>
+            );
+          }} 
+          showsVerticalScrollIndicator={false}
+          dropdownStyle={styles.dropdownMenuStyle}
+          />
 
-          <Button onPress={null} type="submit" title="Submit"/>
-       
-      
-
-    
-
-
-
-
-
-    {/*
-        <Text style={{ fontSize: 30 }}>This is a modal!</Text>
-        
-        <Text>Programme name</Text>
-        <TextInput ></TextInput>
-
-        <Text>Programme durartion</Text>
-        <TextInput keyBoardType="numeric" 
-        placeholder="0-12"
-        onChangeText={(text)=>}></TextInput>
-
-        <Button  onPress={() => navigation.goBack()} title="Dismiss" />
-        
-    */}
-    
+        <Button onPress={() => handleSubmit()} type="submit" title="Submit"/>
     </View>
-        </Formik>
-
 
 
     );
   }
+
+
+  
