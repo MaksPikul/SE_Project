0c02f55025015ef74a025dace6ad081aee45081b
OmarAli003 <omar.ali003@outlook.com>
2024-03-31 18:25:01 +0100

    Working fetch for leaderboard

---
+++ components/LeaderboardItem.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+const LeaderboardItem = ({index, step} : {index:any, step:any}) =>{
+    
+    return(
+        <View style={styles.mainView}>
+            <View style={styles.Item}>
+                <Text style={styles.itemText}>{step.first_name}</Text>
+                <Text style={styles.itemText}>{step.steps} steps</Text>
+                <Text style={styles.itemText}>{index}</Text>
+            </View>
+
+        </View>
+    )
+}
+
+export default LeaderboardItem
+
+const styles = StyleSheet.create({
+    Item:{
+        height:80,
+        width:'100%',
+        borderWidth:2,
+        //backgroundColor:'pink',
+        borderRadius:15,
+        alignItems:'center',
+        justifyContent:'space-evenly',
+        flexDirection: 'row',
+        fontWeight: 'bold',
+    },
+    mainView:{
+        width:'90%',
+        alignItems:'center',
+        justifyContent:'center',
+        //backgroundColor:'red'
+    },
+    itemText:{
+        fontWeight:'bold',
+        fontSize:16
+    }
+})
