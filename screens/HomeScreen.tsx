18fd7dc5e1f0610c39ac690767b437fba685a6a4
admin <jack.thomason114@gmail.com>
2024-04-03 20:19:10 +0100

    Basic Login functionality

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -2,6 +2,8 @@ import {View, Text, StyleSheet , Button} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import HomeButton from '../components/homeButton';
 import { homeStyle } from "../styles/allStyles";
+import { logout } from "../components/Logout";
+import { useLogin } from "../context/loginProvider";
 
 /* 
 For this page
@@ -21,7 +23,12 @@ For all sections,
 */
 
 export default function HomeScreen({}) {
+    const {setIsLoggedIn} = useLogin();
     const navigation = useNavigation()
+    console.log("Logout pressed")
+    const handleLogout = async () => {
+        await logout(); // Call the logout function
+      };
     return(
         <View style={homeStyle.container}>
 
@@ -49,6 +56,13 @@ export default function HomeScreen({}) {
             <HomeButton
             onPress={() => navigation.navigate("Leaderboard")}
             title="Leaderboard"
+            buttonStyle={homeStyle.button}
+            textStyle={homeStyle.buttonText}
+            />
+
+            <CustomButton
+            onPress={() => { handleLogout(), setIsLoggedIn(false)}}
+            title="Logout"
             buttonStyle={homeStyle.button}
             textStyle={homeStyle.buttonText}
             />
