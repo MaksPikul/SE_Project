0a4288bb91c08dccac7d2e2b61424a5b6dbef793
Rahat Dewan <rahat.id03@gmail.com>
2024-04-05 17:05:52 +0100

    Update HomeScreen.tsx

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,56 +1,93 @@
-import {View, Text, StyleSheet , Button} from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import React from 'react';
+import { View, Text, ScrollView,StyleSheet } from 'react-native';
+import { useNavigation} from '@react-navigation/native';
 import CustomButton from '../components/homeButton';
-import { homeStyle } from "../styles/allStyles";
 
-/* 
-For this page
-    there will be a top section with a user icon saying hello name
-*/
+export default function HomeScreen() {
+    const navigate = useNavigation();
 
+    const getUserName = () => {
+        return "user"; // actual user's name
+    };
 
-/*
-For all sections,
-    Default header has to go
-    footer might be good
-    allowing for scrolling would be good
-*/
+    return (
+        <View style={{ flex: 1 }}>
+            {/* Main content with buttons */}
+            <ScrollView contentContainerStyle={homeStyle.container}>
+                <CustomButton
+                    onPress={() => navigate.navigate('FitnessScreen')} // Corrected
+                    title="Fitness"
+                    buttonStyle={homeStyle.button}
+                    textStyle={homeStyle.buttonText}
+                />
 
-export default function HomeScreen({}) {
-    const navigation = useNavigation()
-    return(
-        <View style={homeStyle.container}>
+                <CustomButton
+                    onPress={() => navigate.navigate('NutritionScreen')}
+                    title="Nutrition"
+                    buttonStyle={homeStyle.button}
+                    textStyle={homeStyle.buttonText}
+                />
 
-            <CustomButton
-            onPress={() => navigation.navigate("Fitness")}
-            title="Fitness"
-            buttonStyle={homeStyle.button}
-            textStyle={homeStyle.buttonText}
-            />
+                <CustomButton
+                    onPress={() => navigate.navigate('BlogScreen')}
+                    title="Blog"
+                    buttonStyle={homeStyle.button}
+                    textStyle={homeStyle.buttonText}
+                />
 
-            <CustomButton
-            onPress={() => navigation.navigate("Nutrition")}
-            title="Nutrition"
-            buttonStyle={homeStyle.button}
-            textStyle={homeStyle.buttonText}
-            />
+                <CustomButton
+                    onPress={() => navigate.navigate('AmbassadorPostScreen')}
+                    title="Ambassador"
+                    buttonStyle={homeStyle.button}
+                    textStyle={homeStyle.buttonText}
+                />
+                <CustomButton
+                    onPress={() => navigate.navigate('FitnessPostScreen')}
+                    title="Fitness"
+                    buttonStyle={homeStyle.button}
+                    textStyle={homeStyle.buttonText}
+                />
+                <CustomButton
+                    onPress={() => navigation.navigate('SavedPostSCreen')}
+                    title="Saved post"
+                    buttonStyle={homeStyle.button}
+                    textStyle={homeStyle.buttonText}
+                />
+                <CustomButton
+                    onPress={() => navigation.navigate('RecipiepostScreen')}
+                    title="Recipe"
+                    buttonStyle={homeStyle.button}
+                    textStyle={homeStyle.buttonText}
+                />
+            </ScrollView>
 
-            <CustomButton
-            onPress={() => navigation.navigate("Blog")}
-            title="Blog"
-            buttonStyle={homeStyle.button}
-            textStyle={homeStyle.buttonText}
-            />
-
-            <CustomButton
-            onPress={() => navigation.navigate("Leaderboard")}
-            title="Leaderboard"
-            buttonStyle={homeStyle.button}
-            textStyle={homeStyle.buttonText}
-            />
-
+            {/* Footer */}
+            <View style={homeStyle.footer}>
+                <Text style={homeStyle.footerText}>Footer Content Here</Text>
+            </View>
         </View>
-
-    )
+    );
 }
 
+const homeStyle = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: "column",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: 75,
+    },
+    button: {
+        width: 400,
+        height: 150,
+        backgroundColor: "black",
+        
+        alignItems: "center"
+    },
+    buttonText: {
+        color: "white",
+        fontSize: 16
+
+    }
+    
+})
