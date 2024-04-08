47b2c2686daf4c06fea6b96702ea2824e504a75a
Maks <mmaks522@gmail.com>
2024-04-08 14:07:48 +0100

    Styled home, cant get it to translate -6 on press

--- components/homeButton.jsx
+++ components/homeButton.jsx
@@ -15,17 +15,49 @@
 //trial but check if it runs and it doesnt want to work for me 
 
 import React from 'react';
-import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { Pressable, Text, View, StyleSheet } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
 
+const HomeButton = ({ onPress, title}) => {
 
-const HomeButton = ({ onPress, title, buttonStyle, textStyle }) => {
+  
+  
   return (
-    <TouchableOpacity onPress={onPress} style={buttonStyle}>
-      <Text style={textStyle}>{title}</Text>
-    </TouchableOpacity>
+    <View style={{...bStyles.buttonBack, ...bStyles.button}}>
+      <Pressable
+      onPress={onPress} 
+      style={({pressed})=> [{transform: pressed ? [{translateY: -6}] : null},bStyles.buttonFront]}>  
+        
+        <Text style={{color: "white", fontSize: 19}}>{title}</Text>
+
+      </Pressable>
+    </View>
   );
 };
 
+const bStyles = StyleSheet.create({
+  button:{
+  marginVertical:40,
+  marginHorizontal: 20,
+  },
+  buttonFront:{
+  backgroundColor:"blue",
+  width: 150,
+  height: 140,
+  padding:20,
+  transform: [{translateY:10}],
+  borderRadius:20
+  },
+  buttonBack:{
+  backgroundColor: "navy",
+  width: 140,
+  height: 140,
+  padding:20,
+  alignItems:"center",
+  justifyContent:"center",
+  borderRadius:20
+  }
+})
 
 export default HomeButton;
